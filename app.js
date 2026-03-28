631aaee789cd8755481e110d5c9c3438ab04db47
abdulloh <abdullohmamasiddiov009@gamil.com>
2026-03-28 16:00:47 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+// 41
+// function bolinsin(x, y) {
+//   if (x % y === 0) { return true;
+//   }else
+//     return false;
+
+// }
+
+// console.log(bolinsin(98, 7));
+// console.log(bolinsin(88, 7));
+// ======================================
+//42
+// function raqam(str) {
+//   return Number(str)
+// }
+// console.log(raqam('537677'));
+// =========================================
+//43
+// function tortYuzi(a, b) {
+//   if (a > 0 && b > 0) {
+//     return a * b;
+//   } else {
+//     return -1;
+//   }
+// }
+// console.log(tortYuzi(12,17));
+// console.log(tortYuzi(-12,17));
+
+// ===========================================================
+//44
+// function ismfam(ism, familya) {
+//   return ism + " " + familya;
+// }
+// console.log(ismfam('hzxbs', 'shxvsh'));
+// ================================================
+//46
+// function elements(massiv, soni) {
+//   return massiv.slice(soni);
+// }
+
+// console.log(elements([1, 2, 3, 4, 5],3));
+// =====================================================
+//47
+// function ovozlar(berganlar, bermaganlar) {
+//   return berganlar - bermaganlar;
+// }
+
+// console.log(ovozlar(18, 12));
+// console.log(ovozlar(16, 18));
+// console.log(ovozlar(18, 18));
+// ================================================
+// 48
+// function negativ(son) {
+//   if (son > 0) {
+//     return -son;
+//   } else {
+//     return 0;
+//   }
+// }
+// console.log(negativ(14));
+// console.log(negativ(0));
+// console.log(negativ(10));
+// ========================================
+// 50
+// function kinogaKirish(yosh, otaOna) {
+//   if (yosh >= 15 && otaOna == true) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(kinogaKirish(16, true));
+// console.log(kinogaKirish(13));
+// console.log(kinogaKirish(false));
+// ======================================
